a9750fa2f38e5c7aa0f65c13c23c2d771d797e7e
Asdergh <ramzanhac2005@gmail.com>
2024-03-20 00:29:27 +0300

    some changes were added

--- copter_simulation/contrall_sistem.py
+++ copter_simulation/contrall_sistem.py
@@ -11,9 +11,15 @@ from mechanic_sistem import MechSistemCalculation
 
 class ContrallSistem():
 
-    def __init__(self, kp_position, ki_position, kd_position,
-                 kp_orientation, ki_orientation, kd_orientation,
-                 kp_ang_vel, ki_ang_vel, kd_ang_vel, 
+    def __init__(self, kp_pos_z, ki_pos_z, kd_pos_z,
+                 kp_pos_y, ki_pos_y, kd_pos_y,
+                 kp_pos_x, ki_pos_x, kd_pos_x,
+                 kp_theta, ki_theta, kd_theta,
+                 kp_gamma, ki_gamma, kd_gamma,
+                 kp_psi, ki_psi, kd_psi,
+                 kp_ang_vel_x, ki_ang_vel_x, kd_ang_vel_x,
+                 kp_ang_vel_y, ki_ang_vel_y, kd_ang_vel_y,
+                 kp_ang_vel_z, ki_ang_vel_z, kd_ang_vel_z, 
                  saturation_thred_contour_sup, saturation_thred_contour_inf, 
                  saturation_first_contour_sup=0, saturation_second_contour_sup=0, saturation_first_contour_inf=0, 
                  saturation_second_contour_inf=0):
@@ -24,92 +30,95 @@ class ContrallSistem():
                                                      moment_of_inertia_z=0.001000000000000)
         self.thrust_signal = -1
 
-        self.position_error = np.zeros(2)
-        self.orientation_error = np.zeros(2)
-        self.anguler_vel_error = np.zeros(3)
-
-        self.position_past_error = np.zeros(2)
-        self.orientation_past_error = np.zeros(2)
-        self.anguler_vel_past_error = np.zeros(3)
-
-        self.position_integr_error = np.zeros(2)
-        self.orientation_integr_error = np.zeros(2)
-        self.anguler_vel_integr_error = np.zeros(3)
-
-        self.kp_position = kp_position
-        self.ki_position = ki_position
-        self.kd_position = kd_position
-
-        self.kp_orientation = kp_orientation
-        self.ki_orientation = ki_orientation
-        self.kd_orientation = kd_orientation
-
-        self.kp_ang_vel = kp_ang_vel
-        self.ki_ang_vel = ki_ang_vel
-        self.kd_ang_vel = kd_ang_vel
-
-        self.first_contour_saturation_sup = saturation_first_contour_sup
-        self.second_contour_saturation_sup = saturation_second_contour_sup
-        self.thred_contour_saturation_sup = saturation_thred_contour_sup
-
-        self.first_contour_saturation_inf = saturation_first_contour_inf
-        self.second_contour_saturation_inf = saturation_second_contour_inf
-        self.thred_contour_saturation_inf = saturation_thred_contour_inf
-
-        self.cmd_rotors_anguler_vel = [0.0, 0.0, 0.0, 0.0]
-
-
-
-    def _PID_first_contour(self):
-
-        self.position_error = self.mechanic_sistem.desired_position[:-1] - self.mechanic_sistem.copter_position[:-1]
-        self.thrust_signal = self.mechanic_sistem.desired_position[-1] - self.mechanic_sistem.copter_position[-1]
-        self.position_integr_error += self.position_error[:-1] * self.mechanic_sistem.virt_time
-
-        self.first_contour_P_reg = self.kp_position * self.position_error
-        self.first_contour_I_reg = self.ki_position * self.position_integr_error
-        self.first_contour_D_reg = self.kd_position * ((self.position_error - self.position_past_error) / self.mechanic_sistem.virt_time)
-
-        self.first_contour_signal = (self.first_contour_P_reg + self.first_contour_I_reg + self.first_contour_D_reg)
-        self.position_past_error = self.position_error
-
-    def _PID_second_contour(self):
-
-        self.orientation_error = self.first_contour_signal - self.mechanic_sistem.copter_anguler_orientation[:-1]
-        self.orientation_integr_error = self.orientation_error * self.mechanic_sistem.virt_time
-
-        self.second_contour_P_reg = self.kp_orientation * self.orientation_error
-        self.second_contour_I_reg = self.ki_orientation * self.orientation_integr_error
-        self.second_contour_D_reg = self.kd_orientation * ((self.orientation_error - self.orientation_past_error) / self.mechanic_sistem.virt_time)
-
-        self.second_contour_signal = (self.second_contour_P_reg + self.second_contour_I_reg + self.second_contour_D_reg)
-        self.orientation_past_error = self.orientation_error
-
-    def _PID_thred_contour(self):
-
-        self.anguler_vel_error[:-1] = self.second_contour_signal - self.mechanic_sistem.copter_anguler_velocity[:-1]
-        self.anguler_vel_error[-1] = self.mechanic_sistem.copter_anguler_orientation[-1] - self.mechanic_sistem.copter_anguler_velocity[-1]
-        self.anguler_vel_integr_error[:-
-                                      1] = self.anguler_vel_error[:-1] * self.mechanic_sistem.virt_time
-        self.anguler_vel_integr_error[-1] = self.anguler_vel_error[-1] * self.mechanic_sistem.virt_time
-
-        self.thred_contour_P_reg = self.kp_ang_vel * self.anguler_vel_error
-        self.thred_contour_I_reg = self.ki_ang_vel * self.anguler_vel_integr_error
-        self.thred_contour_D_reg = self.kd_ang_vel * ((self.anguler_vel_error - self.anguler_vel_error) / self.mechanic_sistem.virt_time)
-
-        self.thred_contour_signal = (self.thred_contour_P_reg + self.thred_contour_D_reg + self.thred_contour_I_reg)
-
-        self.thred_contour_signal[0] = self._saturation(self.thred_contour_signal[0],
-                self.thred_contour_saturation_sup,
-                self.thred_contour_saturation_inf)
-        self.thred_contour_signal[1] = self._saturation(self.thred_contour_signal[1],
-                self.thred_contour_saturation_sup,
-                self.thred_contour_saturation_inf)
-        self.thred_contour_signal[2] = self._saturation(self.thred_contour_signal[2],
-                self.thred_contour_saturation_sup,
-                self.thred_contour_saturation_inf)
+        self.kp_pos_z = kp_pos_z
+        self.ki_pos_z = ki_pos_z
+        self.kd_pos_z = kd_pos_z
         
-        self.anguler_vel_past_error = self.anguler_vel_error
+        self.kp_pos_y = kp_pos_y
+        self.ki_pos_y = ki_pos_y
+        self.kd_pos_y = kd_pos_y
+
+        self.kp_pos_x = kp_pos_x
+        self.ki_pos_x = ki_pos_x
+        self.kd_pos_x = kd_pos_x
+
+        self.kp_theta = kp_theta
+        self.ki_theta = ki_theta
+        self.kd_theta = kd_theta
+
+        self.kp_gamma = kp_gamma
+        self.ki_gamma = ki_gamma
+        self.kd_gamma = kd_gamma
+
+        self.kp_psi = kp_psi
+        self.ki_psi = ki_psi
+        self.kd_psi = kd_psi
+
+        self.kp_ang_vel_x = kp_ang_vel_x
+        self.ki_ang_vel_x = ki_ang_vel_x
+        self.kd_ang_vel_x = kd_ang_vel_x
+
+        self.kp_ang_vel_y = kp_ang_vel_y
+        self.ki_ang_vel_y = ki_ang_vel_y
+        self.kd_ang_vel_y = kd_ang_vel_y
+
+        self.kp_ang_vel_z = kp_ang_vel_z
+        self.ki_ang_vel_z = ki_ang_vel_z
+        self.kd_ang_vel_z = kd_ang_vel_z
+
+        self.position_z_error = 0
+        self.position_z_error_past = 0
+        self.position_z_integr_error = 0
+
+        self.position_y_error = 0
+        self.position_y_error_past = 0
+        self.position_y_integr_error = 0
+
+        self.position_x_error = 0
+        self.position_x_error_past = 0
+        self.position_x_integr_error = 0
+
+        self.theta_error = 0
+        self.theta_error_past = 0
+        self.theta_integr_error = 0
+
+        self.gamma_error = 0
+        self.gamma_error_past = 0
+        self.gamma_error_integr_error = 0
+
+        self.psi_error = 0
+        self.psi_error_past = 0
+        self.psi_error_integr_error = 0
+
+        self.ang_vel_x_error = 0
+        self.ang_vel_x_error_past = 0
+        self.ang_vel_x_integr_error = 0
+
+        self.ang_vel_y_error = 0
+        self.ang_vel_y_error_past = 0
+        self.ang_vel_y_integr_error = 0
+        
+        self.ang_vel_z_error = 0
+        self.ang_vel_z_error_past = 0
+        self.ang_vel_z_error_integr_error = 0
+
+        
+        self.saturatoin_thred_contour_inf = saturation_thred_contour_inf
+        self.saturatoin_thred_contour_sup = saturation_thred_contour_sup
+
+
+
+        
+        self.kp_pos_z = 0
+
+
+    def _PID_position(self):
+
+        
+
+
+
+    
 
     def _saturation(self, signal, sup, inf):
 
